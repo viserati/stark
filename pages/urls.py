2af4fa056784b3744f6a9655ab25141cb6dcc25e
Keith Turner <turner@keithturner.com>
2020-05-26 11:36:15 -0600

    Heroku fix

--- pages/urls.py
+++ pages/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 
-from .views import HomePageView, AboutPageView
+from .views import AboutPageView, HomePageView,
 
 urlpatterns = [
     path('about/', AboutPageView.as_view(), name='about'),
